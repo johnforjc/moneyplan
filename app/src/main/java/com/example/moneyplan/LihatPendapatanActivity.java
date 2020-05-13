580b1e73f46b2b506d660e5b73347bd1a5bc62db
JohnforJC <animeloverz072@gmail.com>
2020-05-13 22:14:09 +0700

    Fix lihat pedapatan dan lihat pengeluaran sudah bisa pilih bulan

--- app/src/main/java/com/example/moneyplan/LihatPendapatanActivity.java
+++ app/src/main/java/com/example/moneyplan/LihatPendapatanActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 public class LihatPendapatanActivity extends AppCompatActivity {
     SQLiteDatabase sqLiteDatabase;
@@ -13,37 +16,64 @@ public class LihatPendapatanActivity extends AppCompatActivity {
     Cursor cursor;
     ListView listView;
     MyObject_ListAdapter listAdapter;
+    Button myButton;
+    Spinner mySpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lihat_pendapatan);
+        mySpinner = (Spinner) findViewById(R.id.sp_pendapatan);
+        myButton = (Button) findViewById(R.id.button3);
 
-        listView = (ListView) findViewById(R.id.pendapatan_lv);
-        dbHelper = new DatabaseHelper(getApplicationContext());
-        sqLiteDatabase = dbHelper.getReadableDatabase();
-        cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='true'", null);
-        listAdapter = new MyObject_ListAdapter(getApplicationContext(), R.layout.rowdata);
-        listView.setAdapter(listAdapter);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int seleted = mySpinner.getSelectedItemPosition();
+                String urutan = "";
 
-        if(cursor.moveToFirst())
-        {
-            MyObject awal = new MyObject("Tanggal", "Nama Catatan", "Jumlah");
-            listAdapter.add(awal);
 
-            do{
-                String tanggal, namaCatatan, jumlah;
 
-                tanggal = cursor.getString(2);
-                namaCatatan = cursor.getString(1);
-                jumlah = cursor.getString(4);
+                if(seleted == 0) urutan="01";
+                else if(seleted == 1) urutan="02";
+                else if(seleted == 2) urutan="03";
+                else if(seleted == 3) urutan="04";
+                else if(seleted == 4) urutan="05";
+                else if(seleted == 5) urutan="06";
+                else if(seleted == 6) urutan="07";
+                else if(seleted == 7) urutan="08";
+                else if(seleted == 8) urutan="09";
+                else if(seleted == 9) urutan="10";
+                else if(seleted == 10) urutan="11";
+                else if(seleted == 11) urutan="12";
 
-                MyObject data = new MyObject(tanggal, namaCatatan, jumlah);
+                listView = (ListView) findViewById(R.id.pendapatan_lv);
+                dbHelper = new DatabaseHelper(getApplicationContext());
+                sqLiteDatabase = dbHelper.getReadableDatabase();
+                cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='true' AND  Tanggal BETWEEN '2020-"+ urutan +"-01' AND '2020-"+ urutan +"-31'  ", null);
+                listAdapter = new MyObject_ListAdapter(getApplicationContext(), R.layout.rowdata);
+                listView.setAdapter(listAdapter);
 
-                listAdapter.add(data);
+                if(cursor.moveToFirst())
+                {
+                    MyObject awal = new MyObject("Tanggal", "Nama Catatan", "Jumlah");
+                    listAdapter.add(awal);
 
-            }while(cursor.moveToNext());
-        }
+                    do{
+                        String tanggal, namaCatatan, jumlah;
+
+                        tanggal = cursor.getString(2);
+                        namaCatatan = cursor.getString(1);
+                        jumlah = cursor.getString(4);
+
+                        MyObject data = new MyObject(tanggal, namaCatatan, jumlah);
+
+                        listAdapter.add(data);
+
+                    }while(cursor.moveToNext());
+                }
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/moneyplan/LihatPengeluaran.java
+++ app/src/main/java/com/example/moneyplan/LihatPengeluaran.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class LihatPengeluaran extends AppCompatActivity {
     SQLiteDatabase sqLiteDatabase;
@@ -13,38 +17,62 @@ public class LihatPengeluaran extends AppCompatActivity {
     Cursor cursor;
     ListView listView;
     MyObject_ListAdapter listAdapter;
-
-
+    Spinner mySpinner;
+    Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lihat_pengeluaran);
+        mySpinner = (Spinner) findViewById(R.id.sp_name);
+        myButton = (Button) findViewById(R.id.button4);
 
-        listView = (ListView) findViewById(R.id.pengeluaran_lv);
-        dbHelper = new DatabaseHelper(getApplicationContext());
-        sqLiteDatabase = dbHelper.getReadableDatabase();
-        cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='false' AND  Tanggal BETWEEN '01-04-2020' AND '30-04-2020'  ", null);
-        listAdapter = new MyObject_ListAdapter(getApplicationContext(), R.layout.rowdata);
-        listView.setAdapter(listAdapter);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int seleted = mySpinner.getSelectedItemPosition();
 
-        if(cursor.moveToFirst())
-        {
-            MyObject awal = new MyObject("Tanggal", "Nama Catatan", "Jumlah");
-            listAdapter.add(awal);
-            do{
-                String tanggal, namaCatatan, jumlah;
+                listView = (ListView) findViewById(R.id.pengeluaran_lv);
+                dbHelper = new DatabaseHelper(getApplicationContext());
+                sqLiteDatabase = dbHelper.getReadableDatabase();
 
-                tanggal = cursor.getString(2);
-                namaCatatan = cursor.getString(1);
-                jumlah = cursor.getString(4);
+                if(seleted == 0) cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='false' AND  Tanggal BETWEEN '2020-01-01' AND '2020-01-2020'  ", null);
+                else if(seleted == 2) cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='false' AND  Tanggal BETWEEN '2020-03-01' AND '2020-03-31'  ", null);
+                else if(seleted == 1) cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='false' AND  Tanggal BETWEEN '2020-02-01' AND '2020-02-31'  ", null);
+                else if(seleted == 3) cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='false' AND  Tanggal BETWEEN '2020-04-01' AND '2020-04-31'  ", null);
+                else if(seleted == 4) cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='false' AND  Tanggal BETWEEN '2020-05-01' AND '2020-05-31'  ", null);
+                else if(seleted == 5) cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='false' AND  Tanggal BETWEEN '2020-06-01' AND '2020-06-31'  ", null);
+                else if(seleted == 6) cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='false' AND  Tanggal BETWEEN '2020-07-01' AND '2020-07-31'  ", null);
+                else if(seleted == 7) cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='false' AND  Tanggal BETWEEN '2020-08-01' AND '2020-08-31'  ", null);
+                else if(seleted == 8) cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='false' AND  Tanggal BETWEEN '2020-09-01' AND '2020-09-31'  ", null);
+                else if(seleted == 9) cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='false' AND  Tanggal BETWEEN '2020-10-01' AND '2020-10-31'  ", null);
+                else if(seleted == 10) cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='false' AND  Tanggal BETWEEN '2020-11-01' AND '2020-11-31'  ", null);
+                else if(seleted == 11) cursor =sqLiteDatabase.rawQuery("SELECT * FROM money_note WHERE income='false' AND  Tanggal BETWEEN '2020-12-01' AND '2020-12-31'  ", null);
 
-                MyObject data = new MyObject(tanggal, namaCatatan, jumlah);
+//                Toast.makeText(getApplicationContext(), "urutan "+ urutan, Toast.LENGTH_LONG).show();
 
-                listAdapter.add(data);
+                listAdapter = new MyObject_ListAdapter(getApplicationContext(), R.layout.rowdata);
+                listView.setAdapter(listAdapter);
 
-            }while(cursor.moveToNext());
-        }
+                if(cursor.moveToFirst())
+                {
+                    MyObject awal = new MyObject("Tanggal", "Nama Catatan", "Jumlah");
+                    listAdapter.add(awal);
+                    do{
+                        String tanggal, namaCatatan, jumlah;
+
+                        tanggal = cursor.getString(2);
+                        namaCatatan = cursor.getString(1);
+                        jumlah = cursor.getString(4);
+
+                        MyObject data = new MyObject(tanggal, namaCatatan, jumlah);
+
+                        listAdapter.add(data);
+
+                    }while(cursor.moveToNext());
+                }
+            }
+        });
     }
 
     @Override
